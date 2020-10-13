5fa0aa63539e841176ecddf84e7f6add5dc94fda
ProblemSPb <ussr83@mail.ru>
2020-10-13 13:38:13 +0200

    Add files via upload

---
+++ taskzero.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ */
+
+const TEST_CONSTANTA = 'asd';
+$user_name = "Igor";
+
+/**
+ *  Function does something
+ *
+ * @return String
+ */
+function showSomething()
+{
+    return 'SOMETHING';
+}
+
+if (1==1) {
+    echo 'hi';
+}
